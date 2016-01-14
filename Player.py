66afcba50f50cd653e391705081aa5ee9ee652c5
ubuntu-14.04.3 <xujiacao@gmail.com>
2016-01-13 23:07:28 -0800

    added the bomb class

--- Player.py
+++ Player.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from Bomb import Bomb
 
 #1->left is pressed, 2->up, 3->right, 4->down
 def ChangeNextIndex(current, key):
@@ -52,15 +53,19 @@ class Player:
     image_y = 73
 
     #image in the order of up, down, left, right
-    def __init__(self, image_names, speed,x,y, max_bomb, bomb_damage):
+    def __init__(self, image_names, bomb_name,speed,x,y, max_bomb, bomb_damage):
         if(len(image_names)!=16):
             print "incorrect size of the iamge_names\n"
+        
         self.images = []
         for names in image_names:
             temp = pygame.image.load(names).convert_alpha()
             temp = pygame.transform.scale(temp, (temp.get_width()/2,
                                                  temp.get_height()/2))
             self.images.append(temp)
+        
+        self.bomb_name = bomb_name
+
         self.speed = speed
         self.x = x;
         self.y =y;
@@ -70,14 +75,24 @@ class Player:
         self.image_index = 4
         self.time = 0
 
+        #used to restirct putting too many bomb at a moment
+        self.bomb_since_last = 0
 
-    def Action(self, screen, pressed_Key,seconds):
+
+    def Action(self, screen, pressed_Key,seconds, bomb_queue):
         self.time+=seconds
+        self.bomb_since_last+=seconds
         switch = False
-        if self.time >= 0.4:
+        if self.time >= 0.2:
             switch = True
             self.time = 0
+
         distance = seconds * self.speed
+
+        if pressed_Key[K_SPACE] and self.bomb_since_last>=0.5:
+            self.bomb_since_last = 0
+            temp_bomb = Bomb(self.bomb_name,self.x,self.y)
+            bomb_queue.append(temp_bomb)
         if pressed_Key[K_LEFT]:
             self.x-=distance
             if switch == True or self.image_index<8 or self.image_index>11:
--- Test.py
+++ Test.py
@@ -42,42 +42,45 @@ player_images = [player_up1,player_up2,player_up3,player_up4,
                  player_left1,player_left2,player_left3,player_left4,
                  player_right1,player_right2,player_right3,player_right4]
 
-p = Player(player_images,50,10,20,1,1)
+p = Player(player_images,bomb_image,80,10,20,1,1)
 
-
-bomb_x = 0
-bomb_y = 10
-bomb_placed = False
-count_down = 300
-burst_happened = False
-burst_x = 0
-burst_y = 10
-burst_count_down = 100
+bomb_queue = []
 total_time = 0.05
 current_time = 0.0
+exploded_queue = []
+
 while True:    
     time_passed = clock.tick()
     time_passed_seconds = time_passed / 1000.0
     current_time+=time_passed_seconds
 
-    if(current_time>=total_time):
-        current_time = 0.0
-    else:
+
+    if current_time<total_time:
         continue
+                    
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
-        if event.type!=USEREVENT +1:
-            continue
     
 
     screen.blit(background, (0,0))
 
+    for b in bomb_queue:
+        if b.TimePassed(current_time) == True :
+            screen.blit(burst,(b.GetX(),b.GetY()))
+            bomb_queue.remove(b)
+        else:
+            screen.blit(b.GetImage(),(b.GetX(),b.GetY()))
+
+
     pressed_Key = pygame.key.get_pressed()
 
-    p.Action(screen,pressed_Key,0.1)
-    
-        
+    #third argument pass how many time hada passed since last tiem
+    p.Action(screen,pressed_Key,current_time, bomb_queue)
+
+    #Reset current time
+    current_time = 0.0
+
     pygame.display.update()
 
 
